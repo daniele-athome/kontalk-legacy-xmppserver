b4cefb9c9354d4fb50572c9e75c518e519ec6c55
Daniele Ricci <daniele.athome@gmail.com>
2013-07-28 01:05:41 +0200

    Check for JID user part
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- kontalk/xmppserver/component/c2s.py
+++ kontalk/xmppserver/component/c2s.py
@@ -397,7 +397,7 @@ class InitialPresenceHandler(XMPPHandler):
         self.parent.sfactory.check_conflict(sender)
 
         # initial presence from a client connected to another server, clear it from our presence table
-        if sender.host != self.parent.servername and sender.host in self.parent.keyring.hostlist():
+        if sender.user and sender.host != self.parent.servername and sender.host in self.parent.keyring.hostlist():
             log.debug("deleting %s from presence table" % (sender.user, ))
             self.parent.presencedb.delete(sender.user)
 
