cb85bca965b7cd2acf0c19de5cc42f31f8a5980f
Daniele Ricci <daniele.athome@gmail.com>
2014-01-09 10:58:29 +0100

    Change domain name for custom extensions URIs
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- kontalk/xmppserver/xmlstream2.py
+++ kontalk/xmppserver/xmlstream2.py
@@ -55,8 +55,8 @@ NS_XMPP_STANZA_GROUP = 'urn:xmpp:stanza-group'
 NS_XMPP_SERVER_RECEIPTS = 'urn:xmpp:server-receipts'
 NS_XMPP_STORAGE = 'urn:xmpp:storage'
 
-NS_PRESENCE_PUSH = 'http://www.kontalk.org/extensions/presence#push'
-NS_MESSAGE_UPLOAD = 'http://www.kontalk.org/extensions/message#upload'
+NS_PRESENCE_PUSH = 'http://kontalk.org/extensions/presence#push'
+NS_MESSAGE_UPLOAD = 'http://kontalk.org/extensions/message#upload'
 
 XMPP_STAMP_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 
