7620f1b4a4f4143a66b24b435a1c24e3a9f36977
Daniele Ricci <daniele.athome@gmail.com>
2014-04-26 13:06:42 +0200

    A unit test for resolver subscriptions
    This is the very first unit test for Kontalk XMPP server code. Many
    others will follow soon.
    
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

---
+++ test/test_resolver_subscriptions.py
@@ -0,0 +1,59 @@
+
+import unittest
+import demjson
+
+from twisted.words.protocols.jabber import jid
+
+from kontalk.xmppserver.component.resolver import Resolver
+from kontalk.xmppserver import log, util
+
+class TestResolverSubscriptions(unittest.TestCase):
+
+    def setUp(self):
+        # load configuration
+        self.loadConfiguration('../resolver.conf')
+        # init logging
+        log.init(self.config)
+        # create resolver
+        self.resolver = Resolver(self.config)
+        # override methods
+        self.resolver.is_presence_allowed = self._is_presence_allowed
+
+    def _is_presence_allowed(self, jid_from, jid_to):
+        return 1
+
+    def loadConfiguration(self, filename):
+        # load configuration
+        fp = open('../resolver.conf', 'r')
+        self.config = demjson.decode(fp.read(), allow_comments=True)
+        fp.close()
+
+    def tearDown(self):
+        pass
+
+
+    def testSubscribe(self):
+        jid_from = jid.JID('user1@c2s.prime.kontalk.net')
+        jid_to = jid.JID('user2@kontalk.net')
+        gid = util.rand_str(8)
+        self.resolver.subscribe(self.resolver.translateJID(jid_from),
+            self.resolver.translateJID(jid_to), gid, False)
+
+        subscriptions = { jid.JID('user2@kontalk.net') : [ jid.JID('user1@kontalk.net') ] }
+        self.assertDictEqual(self.resolver.subscriptions, subscriptions, 'Subscriptions not maching.')
+
+    def testUnsubscribe(self):
+        # execute subscription first
+        self.testSubscribe()
+
+        jid_from = jid.JID('user1@c2s.prime.kontalk.net')
+        jid_to = jid.JID('user2@kontalk.net')
+        self.resolver.unsubscribe(self.resolver.translateJID(jid_to),
+            self.resolver.translateJID(jid_from))
+
+        self.assertEqual(len(self.resolver.subscriptions), 0, 'Subscriptions not maching.')
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
