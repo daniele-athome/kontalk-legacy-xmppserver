cb2b7f39029f81853b17a0c1075928891cf5823d
Daniele Ricci <daniele.athome@gmail.com>
2014-10-14 17:25:41 +0200

    More unit tests for util
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- kontalk/xmppserver/util.py
+++ kontalk/xmppserver/util.py
@@ -194,7 +194,7 @@ def generate_filename(mime):
 
 def md5sum(filename):
     md5 = hashlib.md5()
-    with open(filename,'rb') as f:
+    with open(filename, 'rb') as f:
         for chunk in iter(lambda: f.read(128*md5.block_size), ''):
             md5.update(chunk)
     return md5.hexdigest()
--- test/test_util.py
+++ test/test_util.py
@@ -1,5 +1,7 @@
 
 import unittest
+import os
+import tempfile
 
 from kontalk.xmppserver import util
 
@@ -21,10 +23,19 @@ class TestUtil(unittest.TestCase):
             'text/vcard': 'vcf',
             'text/plain': 'txt',
             'audio/3gpp': '3gp',
+            'audio/mpeg': 'mp3',
         }
         for mime, ext in mimes.iteritems():
             filename = util.generate_filename(mime)
             self.assertRegexpMatches(filename, 'att[A-Za-z0-9]+\.' + ext)
+
+    def test_md5sum(self):
+        f = tempfile.NamedTemporaryFile(delete=False)
+        f.write('test data')
+        f.close()
+        data = util.md5sum(f.name)
+        self.assertEqual(data, 'eb733a00c0c9d336e65691a37ab54293')
+        os.unlink(f.name)
 
 
 if __name__ == "__main__":
