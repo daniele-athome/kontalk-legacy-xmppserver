82abbbd645e1673ac608b28bfeee5e9f82eb6778
Daniele Ricci <daniele.athome@gmail.com>
2014-10-15 15:34:50 +0200

    Setuptools script, fixing unit tests integration
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Setuptools script for Kontalk XMPP server
+
+from setuptools import setup
+from kontalk.xmppserver import version
+
+setup(
+    name=version.PACKAGE,
+    version=version.VERSION,
+    description=version.NAME,
+    author=version.AUTHORS[0]['name'],
+    author_email=version.AUTHORS[0]['email'],
+    url='http://www.kontalk.org',
+    packages=[
+        'kontalk',
+        'kontalk.xmppserver',
+        'kontalk.xmppserver.component',
+        'kontalk.xmppserver.component.c2s',
+        'kontalk.xmppserver.component.sm',
+        'kontalk.fileserver',
+        'tests',
+    ],
+    scripts=[],  # TODO: c2s.sh, router.sh, ...
+    license='GNU GPL v3',
+    requires=['twisted', 'wokkel', 'oursql', 'demjson', 'pyasn1'],
+    test_suite='tests'
+    #package_data={'': ['data/*.*', 'data/cards/*.jpg']},
+)
